d2763ce4d78cf8e36ff822239089e37533d93b4e
albystory <bonannialberto@live.it>
2020-02-19 00:11:46 +0000

    again

--- Assets/AudioPlayer.cs
+++ Assets/AudioPlayer.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class AudioPlayer : MonoBehaviour
 {
     AudioSource audioSource;
+
+    float startVolume;
+    public float FadeTime = 1;
     // Start is called before the first frame update
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
+        audioSource.playOnAwake = false;
     }
 
     // Update is called once per frame
@@ -18,12 +22,31 @@ public class AudioPlayer : MonoBehaviour
         {
             if (audioSource.isPlaying)
             {
-                audioSource.Stop();
-                
+                StartCoroutine(FadeOut(audioSource, FadeTime));
+
             }
             else
+                
                 audioSource.Play();
 
         }
     }
+    private IEnumerator FadeOut(AudioSource audioSource, float FadeTime)
+    {
+        float startVolume = audioSource.volume;
+
+        while (audioSource.volume > 0)
+        {
+            audioSource.volume -= startVolume * Time.deltaTime / FadeTime;
+
+            yield return null;
+        }
+
+        audioSource.Stop();
+        audioSource.volume = startVolume;
+    }
+
+
+
+
 }
